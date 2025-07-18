2a78a2701da66e7fc7d9d6c1eb70d54ed20f92ac
Val-d-emar <vhot@live.ru>
2025-07-18 13:18:34 +0400

    feat: first test work

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import Card from './Card';
+import type { IItem } from '../../types/interfaces';
+
+// `describe` groups related tests
+describe('Card Component', () => {
+  // `it` or `test` describes a specific test case
+  it('should render pokemon name and description correctly', () => {
+    // 1. Arrange (Preparation): We are creating mock (artificial) data
+    const mockPokemon: IItem = {
+      name: 'Pikachu',
+      url: 'https://pokeapi.co/api/v2/pokemon/25/',
+    };
+
+    // 2. Act (Action): Rendering the component with mock data
+    render(<Card {...mockPokemon} />);
+
+    // 3. Assert (Check): We check that what we expected has appeared on the screen.
+    // We use screen.getByText to search for elements by their text
+    // Regular expression /Pikachu/i makes search case insensitive
+    const nameElement = screen.getByText(/Pikachu/i);
+    const descriptionElement = screen.getByText(/25/i);
+
+    // `expect` - this statement. We expect that the elements are in the document.
+    expect(nameElement).toBeInTheDocument();
+    expect(descriptionElement).toBeInTheDocument();
+  });
+});
